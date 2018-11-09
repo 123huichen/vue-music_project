aadada66cca40ffe0cba063bee3f9be3cefd8ab3
123huichen <978285317@qq.com>
2018-11-09 18:26:56 +0800

    new

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -5,8 +5,10 @@
 </template>
 
 <script>
+import ListView from 'base/listview/listview'
 import {getSingerList} from '../../api/singer'
 import {ERR_OK} from '../../api/config'
+import Singer from 'common/js/singer'
 
 export default {
     data() {
@@ -25,7 +27,52 @@ export default {
                     this.singers = res.data.list
                 }
             })
+        },
+        _normalizeSinger(list) {
+            let map = {
+                hot: {
+                    title: HOT_NAME,
+                    items: []
+                }
+            }
+            list.forEach((item, index) => {
+                if (index < HOT_SINGER_LEN) {
+                    map.hot.items.push(new Singer({
+                        name: item.Fsinger_name,
+                        id: item.Fsinger_mid
+                    }))
+                }
+                const key = item.Findex
+                if (!map[key]) {
+                    map[key] = {
+                        title: key,
+                        items:[]
+                    }
+                }
+                map[key].items.push(new Singer({
+                    name: item.Fsinger_name,
+                    id: item.Fsinger_mid
+                }))
+            })
+            // 为了得到有序列表，我们需要处理 map
+            let ret = []
+            let hot = []
+            for (let key in map) {
+                let val = map[key]
+                if (val.title.match(/[a-zA-Z]/)) {
+                    ret.push(val)
+                }else if (val.title === HOT_NAME) {
+                    hot.push(val)
+                }
+            }
+            ret.sort((a, b) => {
+                return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+            })
+            return hot.concat(ret)
         }
+    },
+    components: {
+      ListView
     }
 }
 </script>
