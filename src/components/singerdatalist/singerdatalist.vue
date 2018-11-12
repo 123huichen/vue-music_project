1f7c88532661dee139c796232c2cfd156d8c56fd
123huichen <978285317@qq.com>
2018-11-12 18:16:24 +0800

    singer

--- src/components/singerdatalist/singerdatalist.vue
+++ src/components/singerdatalist/singerdatalist.vue
@@ -1,5 +1,7 @@
 <template>
-    <div>格式</div>
+    <transition name="slide">
+        <div class="singer-datalist">格式</div>
+    </transition>  
 </template>
 
 <script>
@@ -11,5 +13,19 @@ export default {
 <style lang="stylus" scoped>
 @import "../../common/stylus/variable"
 
+.singer-datalist
+    position :fixed
+    z-index :100
+    top :0px
+    bottom :0px
+    left :0px
+    right :0px
+    background :$color-background
+
+.slide-enter-active, .slide-leave-active
+    transition :all 0.3s
+
+.slide-enter, .slide-leave-to 
+    transform :translate3d(100%, 0, 0)
 
 </style>
