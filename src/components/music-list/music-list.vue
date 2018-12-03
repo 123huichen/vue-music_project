1c576bd084fd3eed6187090d1e66403234c9a3b9
123huichen <978285317@qq.com>
2018-12-03 10:38:38 +0800

    更新

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -17,7 +17,7 @@
         <scroll :data="songs" @scroll="scroll" 
                 :listen-scroll="listenScroll" :probe-type="probeType" class="list" ref="list">
             <div class="song-list-wrapper">
-                <song-list :songs="songs"></song-list>
+                <song-list @select="selectItem" :songs="songs"></song-list>
             </div>
             <div class="loading-container" v-show="!songs.length">
                 <loading></loading>
@@ -30,6 +30,7 @@
 import Scroll from '../../base/scroll/scroll'
 import SongList from '../../base/song-list/song-list'
 import Loading from '../../base/loading/loading'
+import {mapActions} from 'vuex'
 
 const RESERVED_HEIGHT = 40
 // const transform = prefixStyle('transform')
@@ -78,7 +79,17 @@ export default {
         },
         back() {
             this.$router.back()  // back() 方法可加载历史列表中的前一个 URL(如果存在)
-        }
+        },
+        selectItem(item, index) {
+            //console.log(item)
+            this.selectPlye({
+                list: this.songs,
+                index
+            })
+        },
+        ...mapActions([
+            'selectPlay'
+        ])
     },
     watch: {
         scrollY(newVal) {
